ebb8ab242421f00ada2d95c53cfdfabb02140f25
Karanaka <yildirimbilgekaan03@gmail.com>
2026-08-06 13:19:18 +0300

    Game Over Popup implemented

--- ExpoTheExplorer/Assets/Scripts/UI/GameOverPopupView.cs
+++ ExpoTheExplorer/Assets/Scripts/UI/GameOverPopupView.cs
@@ -24,8 +24,6 @@ namespace ExpoTheExplorer.UI
         [SerializeField] private GameManager gameManager;
         [SerializeField] private GameObject popupRoot;
         [SerializeField] private TMP_Text livesRefillText;
-        [SerializeField] private TMP_Text softMoneyCostText;
-        [SerializeField] private TMP_Text gemCostText;
         [SerializeField] private Button softMoneyButton;
         [SerializeField] private Button gemButton;
         [SerializeField] private Button mainMenuButton;
@@ -62,8 +60,6 @@ namespace ExpoTheExplorer.UI
 
             popupRoot.SetActive(true);
             livesRefillText.text = state.MaxLives.ToString();
-            softMoneyCostText.text = config.ContinueSoftMoneyCost.ToString();
-            gemCostText.text = config.ContinueGemCost.ToString();
             softMoneyButton.interactable = state.SoftMoney >= config.ContinueSoftMoneyCost;
             gemButton.interactable = state.Gems >= config.ContinueGemCost;
         }
@@ -92,8 +88,6 @@ namespace ExpoTheExplorer.UI
             if (gameManager == null) missing.Add(nameof(gameManager));
             if (popupRoot == null) missing.Add(nameof(popupRoot));
             if (livesRefillText == null) missing.Add(nameof(livesRefillText));
-            if (softMoneyCostText == null) missing.Add(nameof(softMoneyCostText));
-            if (gemCostText == null) missing.Add(nameof(gemCostText));
             if (softMoneyButton == null) missing.Add(nameof(softMoneyButton));
             if (gemButton == null) missing.Add(nameof(gemButton));
             if (mainMenuButton == null) missing.Add(nameof(mainMenuButton));
